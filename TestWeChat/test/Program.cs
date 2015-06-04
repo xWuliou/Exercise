cf2f025fad12e0c08b7434993d60ddd07cae1aee
xwulitou <xwulitou@live.com>
2015-06-05 00:38:26 +0800

    debug

---
+++ TestWeChat/test/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonText = @"{""input"" : ""value"", ""output"" : ""result""}";
+            JsonReader reader = new JsonTextReader(new StringReader(jsonText));
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.TokenType + "\t\t" + reader.ValueType + "\t\t" + reader.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
